52a5c228a4eef6fd9b9581c055bcf07f8c0cb773
RnD-Indowings <vayam.jain@indowings.com>
2025-02-14 11:14:57 +0530

    Update term.sh

--- term.sh
+++ term.sh
@@ -3,20 +3,20 @@
 previous_pid=""
 
 while true; do
-    # Store the currently running gnome-terminal process before launching a new one
-    previous_pid=$(pgrep -n gnome-terminal)
+    # Find the PID of the last opened gnome-terminal before launching a new one
+    previous_pid=$(ps -eo pid,cmd --sort=start_time | grep "gnome-terminal" | grep -v "grep" | awk 'END{print $1}')
 
-    # Open a new terminal
+    # Open a new GNOME terminal
     gnome-terminal &
-    
-    # Wait for 2 seconds to ensure the new terminal is running
+
+    # Wait 2 seconds to ensure the new terminal starts
     sleep 2
 
-    # Kill the previous terminal, ensuring we don't kill the new one
-    if [ ! -z "$previous_pid" ]; then
+    # Kill the previous terminal **only if it exists**
+    if [[ ! -z "$previous_pid" ]]; then
         kill "$previous_pid" 2>/dev/null
     fi
 
-    # Wait 60 seconds before launching the next terminal
+    # Wait 60 seconds before repeating
     sleep 60
 done
